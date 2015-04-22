af29eff25aa37865e616f3bfc3a6c90da45c8860
AJ ONeal <awesome@coolaj86.com>
2015-04-21 20:13:31 -0600

    better comments

--- build.sh
+++ build.sh
@@ -10,13 +10,16 @@ cat src/lds-io-storage.js \
   > lds-api.js
 echo "Minifying > lds-api.min.js"
 uglifyjs lds-api.js > lds-api.min.js 2>/dev/null || echo "uglifyjs failed"
+echo ""
 
 echo "Building > angular-lds-api.js"
 cat lds-api.js src/angular-lds-api.js > angular-lds-api.js
 echo "Minifying > angular-lds-api.min.js"
 uglifyjs angular-lds-api.js > angular-lds-api.min.js 2>/dev/null || echo "uglifyjs failed"
+echo ""
 
 echo "Building lds-api.jquery.js"
 cat lds-api.js src/lds-api.jquery.js > lds-api.jquery.js
-echo "uglifyjs lds-io.js > lds-io.min.js"
+echo "Minifying > lds-io.min.js"
+echo ""
 uglifyjs lds-api.jquery.js > lds-api.jquery.min.js 2>/dev/null || echo "uglifyjs failed"
