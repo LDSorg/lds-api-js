6966544b19997e1c5c2f2f5c3f2fbdc45eb2e2f5
AJ ONeal <awesome@coolaj86.com>
2015-04-22 01:55:23 -0600

    handle README conflicts

--- publish.sh
+++ publish.sh
@@ -12,8 +12,22 @@ mversion -m "${TYPE}"
 
 git checkout angular
 git merge master
+git checkout master -- README.md
+git checkout master -- README.angular.md
+rm README.generic.md
+mv README.md README.generic.md
+mv README.angular.md README.md
+git reset -- README.angular.md
+git add README.* && git commit -m "merge updates" || true
 git push angular-lds-io angular:master
 
 git checkout jquery
 git merge master
+git checkout master -- README.md
+git checkout master -- README.jquery.md
+rm README.generic.md
+mv README.md README.generic.md
+mv README.jquery.md README.md
+git reset -- README.jquery.md
+git add README.* && git commit -m "merge updates" || true
 git push jquery-lds-io jquery:master
